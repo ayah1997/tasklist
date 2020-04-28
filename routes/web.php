a53334d471b2222829f8997a3396acd24a843183
ayakhader <sayedahmmeda@gmail.com>
2020-04-28 05:49:50 +0300

    first commit

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
 Route::get('/','TaskController@index');
 
 Route::get('task/{id}','TaskController@show');
